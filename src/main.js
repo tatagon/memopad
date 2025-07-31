0d46a85b6c03f8b33626dbce7e1bb0104ab04e29
tatagon <koura2001.jp@gmail.com>
2025-08-01 00:14:17 +0900

    update gnalytics

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
+import VueGtag from 'vue-gtag-next'
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -12,5 +13,12 @@ pinia.use(piniaPluginPersistedstate)
 
 app.use(router)
 app.use(pinia)
+
+app.use(VueGtag, {
+  property: {
+    id: 'G-0F3BTMEC0V',
+    router, // Vue Routerを使っている場合はrouterを渡すと自動でページビューを追跡
+  },
+})
 
 app.mount('#app')
